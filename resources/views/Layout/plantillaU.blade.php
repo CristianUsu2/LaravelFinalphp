c09a0d3d8d36aff8bf94d3c21e2e01c824309547
Daniel <danielcarvagil10@gmail.com>
2021-02-04 21:54:00 -0500

    estilos corregidos

--- resources/views/Layout/plantillaU.blade.php
+++ resources/views/Layout/plantillaU.blade.php
@@ -43,7 +43,7 @@
         <header class="header">
             <div class="contenedor-logo">
                 <button id="boton-menu" class="boton-menu"><i class="fas fa-bars"></i></button>
-                <a href="#" class="logo"><img src="../Usuario/img/logo.jpeg" alt="" width="120"></a>
+                <a href="#" class="logo1"><img src="../Usuario/img/logo.jpeg" alt="" width="120"></a>
             </div>
           <div class="barra-busqueda">
                 <input type="text" placeholder="Buscar">
@@ -192,6 +192,108 @@
       zoomWindowFadeOut: 750
    }
    );
+ </script>
+ <script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+ <script >/* LOGIN - MAIN.JS - dp 2017 */
+ 
+ // LOGIN TABS
+ $(function() {
+   var tab = $('.tabs h3 a');
+   tab.on('click', function(event) {
+     event.preventDefault();
+     tab.removeClass('active');
+     $(this).addClass('active');
+     tab_content = $(this).attr('href');
+     $('div[id$="tab-content"]').removeClass('active');
+     $(tab_content).addClass('active');
+   });
+ });
+ 
+ // SLIDESHOW
+ $(function() {
+   $('#slideshow > div:gt(0)').hide();
+   setInterval(function() {
+     $('#slideshow > div:first')
+     .fadeOut(1000)
+     .next()
+     .fadeIn(1000)
+     .end()
+     .appendTo('#slideshow');
+   }, 3850);
+ });
+ 
+ // CUSTOM JQUERY FUNCTION FOR SWAPPING CLASSES
+ (function($) {
+   'use strict';
+   $.fn.swapClass = function(remove, add) {
+     this.removeClass(remove).addClass(add);
+     return this;
+   };
+ }(jQuery));
+ 
+ // SHOW/HIDE PANEL ROUTINE (needs better methods)
+ // I'll optimize when time permits.
+ $(function() {
+   $('.agree,.forgot, #toggle-terms, .log-in, .sign-up').on('click', function(event) {
+     event.preventDefault();
+     var terms = $('.terms'),
+         recovery = $('.recovery'),
+         close = $('#toggle-terms'),
+         arrow = $('.tabs-content .fa');
+     if ($(this).hasClass('agree') || $(this).hasClass('log-in') || ($(this).is('#toggle-terms')) && terms.hasClass('open')) {
+       if (terms.hasClass('open')) {
+         terms.swapClass('open', 'closed');
+         close.swapClass('open', 'closed');
+         arrow.swapClass('active', 'inactive');
+       } else {
+         if ($(this).hasClass('log-in')) {
+           return;
+         }
+         terms.swapClass('closed', 'open').scrollTop(0);
+         close.swapClass('closed', 'open');
+         arrow.swapClass('inactive', 'active');
+       }
+     }
+     else if ($(this).hasClass('forgot') || $(this).hasClass('sign-up') || $(this).is('#toggle-terms')) {
+       if (recovery.hasClass('open')) {
+         recovery.swapClass('open', 'closed');
+         close.swapClass('open', 'closed');
+         arrow.swapClass('active', 'inactive');
+       } else {
+         if ($(this).hasClass('sign-up')) {
+           return;
+         }
+         recovery.swapClass('closed', 'open');
+         close.swapClass('closed', 'open');
+         arrow.swapClass('inactive', 'active');
+       }
+     }
+   });
+ });
+ 
+ // DISPLAY MSSG
+ $(function() {
+   $('.recovery .button').on('click', function(event) {
+     event.preventDefault();
+     $('.recovery .mssg').addClass('animate');
+     setTimeout(function() {
+       $('.recovery').swapClass('open', 'closed');
+       $('#toggle-terms').swapClass('open', 'closed');
+       $('.tabs-content .fa').swapClass('active', 'inactive');
+       $('.recovery .mssg').removeClass('animate');
+     }, 2500);
+   });
+ });
+ 
+ // DISABLE SUBMIT FOR DEMO
+ $(function() {
+   $('.button').on('click', function(event) {
+     $(this).stop();
+     event.preventDefault();
+     return false;
+   });
+ });
+ //# sourceURL=pen.js
  </script>
   </body>
 </html>
