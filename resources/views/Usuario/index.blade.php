a1f499b7005df876b0b4d4b1d182d7acfbe7bb0c
Daniel Carvajal <danielcarvagil10@gmail.com>
2021-03-23 22:28:50 -0500

    solamente falta estilos al recibir correo, vistas y plantillas listas

--- resources/views/Usuario/index.blade.php
+++ resources/views/Usuario/index.blade.php
@@ -3,6 +3,88 @@
  
             
 
+<div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="slider-wrapper-area">
+                    <div class="hero-slider-active hero__1 slick-dot-style hero-dot">
+                        <div class="single-slider" style="background-image: url(../Usuario/img/slider11_bg.jpg);">
+                            <div class="container p-0">
+                                <div class="slider-main-content">
+                                    <div class="slider-content-img">
+                                        <img src="../Usuario/img/slider11_lable1.png" alt="">
+                                        <img src="../Usuario/img/slider11_lable2.png" alt="">
+                                        <img src="../Usuario/img/slider11_lable3.png" alt="">
+                                    </div>
+                                    <div class="slider-text">
+                                        <div class="slider-label">
+                                            <img src="../Usuario/img/slider11_lable4.png" alt="">
+                                        </div>
+                                        <h1>headphones az12</h1>
+                                        <p>Typi Non Habent Claritatem Insitam; Est Usus Legentis</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single-slider" style="background-image: url(../Usuario/img/slider12_bg.jpg);">
+                            <div class="container p-0">
+                                <div class="slider-main-content">
+                                    <div class="slider-content-img">
+                                        <img src="../Usuario/img/slider11_lable1.png" alt="">
+                                        <img src="../Usuario/img/slider11_lable2.png" alt="">
+                                        <img src="../Usuario/img/slider11_lable3.png" alt="">
+                                    </div>
+                                    <div class="slider-text">
+                                        <div class="slider-label">
+                                            <img src="../Usuario/img/slider11_lable4.png" alt="">
+                                        </div>
+                                        <h1>samson s90</h1>
+                                        <p>Typi Non Habent Claritatem Insitam; Est Usus Legentis</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+    <div class="banner-area mt-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 order-1">
+                    <div class="img-container img-full fix imgs-res mb-sm-30">
+                        <a href="#">
+                            <img src="../Usuario/img/banner_left.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-5 order-sm-3">
+                    <div class="img-container img-full fix mb-30">
+                        <a href="#">
+                            <img src="../Usuario/img/banner_static_top1.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="img-container img-full fix mb-30">
+                        <a href="#">
+                            <img src="../Usuario/img/banner_static_top2.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 order-2 order-md-3">
+                    <div class="img-container img-full fix">
+                        <a href="#">
+                            <img src="../Usuario/img/banner_static_top3.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
 
 
 <div class="page-wrapper pt-6 pb-28 pb-md-6 pb-sm-6 pt-xs-36">
